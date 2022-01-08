57b2bc2faeb891bfcf0f347e0f20038d915f1bb6
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2022-01-08 01:00:48 -0800

    Write and Read file class.

---
+++ TeamCode/src/main/java/teamcode/Season_Setup/File_WriteAndRead.java
@@ -0,0 +1,33 @@
+/* Copied from "https://www.w3schools.com/java/java_files_create.asp"
+ *
+ * Modified by team FIX IT 3491.
+ */
+
+package teamcode.Season_Setup;
+
+import com.qualcomm.robotcore.util.ReadWriteFile;
+
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+
+import java.io.File;
+
+
+public class File_WriteAndRead extends Robot {
+
+    /**
+     * Writing to a file in the Control Hub
+     * @param textToWrite Parameter for what text is written
+     */
+    public static void writeToFile(String textToWrite) {
+        File myFileName = AppUtil.getInstance().getSettingsFile("robotData.txt");
+        ReadWriteFile.writeFile(myFileName, textToWrite);
+    }
+
+    /**
+     * Reading what is on the file in the Control Hub
+     */
+    public static void readFromFile() {
+        File myFileName = AppUtil.getInstance().getSettingsFile("robotData.txt");
+        alliance = ReadWriteFile.readFile(myFileName);
+    }
+}
