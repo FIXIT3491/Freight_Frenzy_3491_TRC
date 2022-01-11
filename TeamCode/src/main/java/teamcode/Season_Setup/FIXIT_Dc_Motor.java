16ac5046a07aa86063b8407d315927ff5545acef
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2022-01-10 16:23:13 -0800

    Updated FIXIT_Dc_Motor class.

--- TeamCode/src/main/java/teamcode/Season_Setup/FIXIT_Dc_Motor.java
+++ TeamCode/src/main/java/teamcode/Season_Setup/FIXIT_Dc_Motor.java
@@ -1,11 +1,14 @@
 package teamcode.Season_Setup;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
 import TrcFtcLib.ftclib.FtcDcMotor;
 
 public class FIXIT_Dc_Motor extends FtcDcMotor
 {
-    private FtcDcMotor slaveMotor;
-    private static final double BACK_WHEEL_POWER_REDUCTION = 0.7559;
+    private final FtcDcMotor slaveMotor;
+    private static final double BACK_WHEEL_POWER_REDUCTION = 0.9449;
 
     public FIXIT_Dc_Motor(String motorName, String slaveWheel)
     {
@@ -16,7 +19,27 @@ public class FIXIT_Dc_Motor extends FtcDcMotor
     @Override
     public void set(double power)
     {
-        super.set(power);
-        slaveMotor.set(power*BACK_WHEEL_POWER_REDUCTION);
+        super.set(power*BACK_WHEEL_POWER_REDUCTION);
+        slaveMotor.set(power);
+    }
+
+    public void setMode(DcMotor.RunMode runMode)
+    {
+        super.motor.setMode(runMode);
+        slaveMotor.motor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    @Override
+    public void setInverted (boolean inverted)
+    {
+        super.setInverted(inverted);
+        slaveMotor.setInverted(inverted);
+    }
+
+    @Override
+    public void setBrakeModeEnabled (boolean enabled)
+    {
+        super.setBrakeModeEnabled(enabled);
+        slaveMotor.setBrakeModeEnabled(enabled);
     }
 }
