1591b0bcab78c8795a293ef9288094acde9ed210
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2022-01-10 18:01:43 -0800

    Added JavaDocs.

--- TeamCode/src/main/java/teamcode/Season_Setup/FIXIT_Dc_Motor.java
+++ TeamCode/src/main/java/teamcode/Season_Setup/FIXIT_Dc_Motor.java
@@ -10,12 +10,23 @@ public class FIXIT_Dc_Motor extends FtcDcMotor
     private final FtcDcMotor slaveMotor;
     private static final double BACK_WHEEL_POWER_REDUCTION = 0.9449;
 
+
+    /**
+     * Create a set of motors to be connected.
+     * @param motorName Primary Motor (Cart Wheel)
+     * @param slaveWheel Secondary Motor (Omni Wheel)
+     */
     public FIXIT_Dc_Motor(String motorName, String slaveWheel)
     {
         super(motorName);
         slaveMotor = new FtcDcMotor(slaveWheel);
     }
 
+    /**
+     * Setting the power reduction to Cart wheel for both wheels to spin at the same Tangential
+     * Velocity.
+     * @param power Power Reduction
+     */
     @Override
     public void set(double power)
     {
@@ -23,12 +34,20 @@ public class FIXIT_Dc_Motor extends FtcDcMotor
         slaveMotor.set(power);
     }
 
+    /**
+     * Setting the runMode for the Primary Motor.
+     * @param runMode Primary Motor's runMode
+     */
     public void setMode(DcMotor.RunMode runMode)
     {
         super.motor.setMode(runMode);
         slaveMotor.motor.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
     }
 
+    /**
+     * Set the motor direction.
+     * @param inverted specifies true to invert motor direction, false otherwise
+     */
     @Override
     public void setInverted (boolean inverted)
     {
@@ -36,6 +55,10 @@ public class FIXIT_Dc_Motor extends FtcDcMotor
         slaveMotor.setInverted(inverted);
     }
 
+    /**
+     * Set the motor brake mode.
+     * @param enabled specifies true to enable brake mode, false otherwise
+     */
     @Override
     public void setBrakeModeEnabled (boolean enabled)
     {
