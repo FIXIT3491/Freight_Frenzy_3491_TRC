360577b90bc3bd36233e3ec08ea6487f595bca82
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2022-02-23 08:41:37 -0800

    Altered Vision Pipeline.
    All errors should be fixed, testing required.

--- TeamCode/src/main/java/teamcode/Season_Setup/Freight_Frenzy_Pipeline.java
+++ TeamCode/src/main/java/teamcode/Season_Setup/Freight_Frenzy_Pipeline.java
@@ -5,206 +5,250 @@ import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 
-public class Freight_Frenzy_Pipeline
-{
-
-    // Variable Declaration
-    public static volatile Pipeline.ElementPosition positionOfTeamShippingElement = Pipeline.ElementPosition.LEFT;
-
-    public static double analysisLeft = 0.0;
-    public static double analysisCenter = 0.0;
-    public static double analysisRight = 0.0;
-
-
     /**
      * Pipeline Class
      */
-    public static class Pipeline extends OpenCvPipeline
+public class Freight_Frenzy_Pipeline extends OpenCvPipeline
+{
+    // Variable declaration
+    public int elementPosition = 0; // 1 = Left (level one,) 2 = Center (level two,) 3 = Right (level three)
+    private static final int ELEMENT_THRESHOLD = 1;
+
+    // Camera View set-up
+    boolean viewportPaused;
+    public OpenCvCamera webcam;
+
+    // Viewport setup
+    @Override
+    public void onViewportTapped()
     {
-        // Camera View set-up
-        boolean viewportPaused;
-        public OpenCvCamera webcam;
+        viewportPaused = !viewportPaused;
 
-        // Viewport setup
-        @Override
-        public void onViewportTapped()
+        if(viewportPaused)
         {
-            viewportPaused = !viewportPaused;
-
-            if(viewportPaused)
-            {
-                webcam.pauseViewport();
-            }
-            else
-            {
-                webcam.resumeViewport();
-            }
+            webcam.pauseViewport();
         }
-
-
-        // An enum to define the Team Shipping Element's position on the Barcode
-        public enum ElementPosition {
-            LEFT,
-            CENTER,
-            RIGHT
-        }
-
-        public ElementPosition elementPosition;
-
-        // Color constant
-        final Scalar WHITE = new Scalar(255, 255, 255);
-        final Scalar GREEN = new Scalar(0, 255, 0);
-
-        // The core values which define the location and size of the sample regions
-        static final int REGION_HEIGHT = 100;        static final int REGION_WIDTH = 70;
-
-        static final int CENTER_BARCODE_TOP_LEFT_X_ANCHOR_POINT = 320/2 - REGION_WIDTH/2;
-
-        static final Point LEFT_BARCODE_TOP_LEFT_ANCHOR_POINT =   new Point(0, 100);
-        static final Point CENTER_BARCODE_TOP_LEFT_ANCHOR_POINT = new Point(CENTER_BARCODE_TOP_LEFT_X_ANCHOR_POINT, 100);
-        static final Point RIGHT_BARCODE_TOP_LEFT_ANCHOR_POINT =  new Point(320-REGION_WIDTH, 100);
-
-
-        ////* Creating the sample regions *////
-        /* pointA = The Top left point of the region */
-        /* pointB = The Bottom right point of the region */
-
-        // Left Barcode
-        static final Rect LEFT_BARCODE = new Rect(
-                new Point(LEFT_BARCODE_TOP_LEFT_ANCHOR_POINT.x,
-                        LEFT_BARCODE_TOP_LEFT_ANCHOR_POINT.y),
-                new Point(LEFT_BARCODE_TOP_LEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                        LEFT_BARCODE_TOP_LEFT_ANCHOR_POINT.y + REGION_HEIGHT));
-
-        // Center Barcode
-        static final Rect CENTER_BARCODE = new Rect(
-                new Point(CENTER_BARCODE_TOP_LEFT_ANCHOR_POINT.x,
-                        CENTER_BARCODE_TOP_LEFT_ANCHOR_POINT.y),
-                new Point(CENTER_BARCODE_TOP_LEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                        CENTER_BARCODE_TOP_LEFT_ANCHOR_POINT.y + REGION_HEIGHT));
-
-        // Right Barcode
-        static final Rect RIGHT_BARCODE = new Rect(
-                new Point(RIGHT_BARCODE_TOP_LEFT_ANCHOR_POINT.x,
-                        RIGHT_BARCODE_TOP_LEFT_ANCHOR_POINT.y),
-                new Point(RIGHT_BARCODE_TOP_LEFT_ANCHOR_POINT.x + REGION_WIDTH,
-                        RIGHT_BARCODE_TOP_LEFT_ANCHOR_POINT.y + REGION_HEIGHT));
-
-        // Variable Declaration
-        Mat leftBarcode;
-        Mat centerBarcode;
-        Mat rightBarcode;
-        Mat mat = new Mat(); // Storing the Colour Channel selected (BGR in this case)
-        Mat bLeft = new Mat(); // Storing the extract "b" (blue) channel for Left
-        Mat bCenter = new Mat(); // Storing the extract "b" (blue) channel for Center
-        Mat bRight = new Mat(); // Storing the extract "b" (blue) channel for Right
-        Mat holdLeft = new Mat(); // Storing left Mat
-        Mat holdCenter = new Mat(); // Storing center Mat
-        Mat holdRight = new Mat(); // Storing right Mat
-        public double leftValue;
-        public double centerValue;
-        public double rightValue;
-
-
-        /**
-         * This function takes the RGB frame, converts to BGR,
-         * and extracts the B channel to the "B" variable
-         * @param input Camera input
-         */
-        void inputToBGR(Mat input) {
-            Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
-
-            // Extract the H channel from HSV
-            Core.extractChannel(mat, bLeft, 0);
-            Core.extractChannel(mat, bCenter, 0);
-            Core.extractChannel(mat, bRight, 0);
-
-            // Limiting to certain Threshold
-            Imgproc.threshold(bLeft, holdLeft, 89, 255, Imgproc.THRESH_BINARY_INV);
-            Imgproc.threshold(bLeft, holdCenter, 89, 255, Imgproc.THRESH_BINARY_INV);
-            Imgproc.threshold(bLeft, holdRight, 89, 255, Imgproc.THRESH_BINARY_INV);
-        }
-
-
-        /**
-         * This function draw a Rectangle that is than displayed on the screen
-         * @param input Camera input
-         */
-        void drawRectangle (Mat input, Rect rect, Scalar colour, int thickness) {
-            // Left Barcode Rectangle
-            Imgproc.rectangle(
-                    input, // Buffer to draw on
-                    rect, // Where the Rectangle is drawn
-                    colour, // The colour of the rectangle is drawn in
-                    thickness); // Thickness of the rectangle lines
-        }
-
-
-        /**
-         * @param input Initializing the frame
-         */
-        @Override
-        public void init(Mat input) {
-            inputToBGR(input);
-            leftBarcode = bLeft.submat(LEFT_BARCODE);
-            centerBarcode = bCenter.submat(CENTER_BARCODE);
-            rightBarcode = bRight.submat(RIGHT_BARCODE);
-        }
-
-
-        @Override
-        public Mat processFrame(Mat input) {
-
-            // Initializing the frame
-            inputToBGR(input);
-
-            // Drawing the Rectangles
-            drawRectangle(input, LEFT_BARCODE, WHITE,2); // Left Barcode Rectangle
-            drawRectangle(input, CENTER_BARCODE, WHITE,2); // Center Barcode Rectangle
-            drawRectangle(input, RIGHT_BARCODE, WHITE,2); // Right Barcode Rectangle
-
-
-            // Setting variable values
-            leftValue = (int) Core.mean(leftBarcode).val[0];
-            centerValue = (int) Core.mean(centerBarcode).val[0];
-            rightValue = (int) Core.mean(rightBarcode).val[0];
-
-
-            // Record out analysis
-            if (leftValue > centerValue && leftValue > rightValue) {
-                elementPosition = ElementPosition.LEFT;
-                drawRectangle(input, LEFT_BARCODE, GREEN,3); // Left Barcode Rectangle
-
-            } else if (centerValue > leftValue && centerValue > rightValue) {
-                elementPosition = ElementPosition.CENTER;
-                drawRectangle(input, CENTER_BARCODE, GREEN,3); // Left Barcode Rectangle
-
-            } else if (rightValue > leftValue && rightValue > centerValue) {
-                elementPosition = ElementPosition.RIGHT;
-                drawRectangle(input, RIGHT_BARCODE, GREEN,3); // Left Barcode Rectangle
-
-            } else {
-                elementPosition = ElementPosition.RIGHT;
-                drawRectangle(input, LEFT_BARCODE, GREEN,3); // Left Barcode Rectangle
-
-            }
-
-
-            // Setting Analysis Value for Telemetry
-            analysisLeft = leftValue;
-            analysisCenter = centerValue;
-            analysisRight = rightValue;
-
-            // Setting Position of Element for Telemetry
-            positionOfTeamShippingElement = elementPosition;
-
-            // Return Input
-            return input;
+        else
+        {
+            webcam.resumeViewport();
         }
     }
+
+    // Color constant
+    final Scalar WHITE = new Scalar(255, 255, 255);
+    final Scalar GREEN = new Scalar(0, 255, 0);
+
+    // The core values which define the location and size of the sample regions
+    static final int REGION_HEIGHT = 100;        static final int REGION_WIDTH = 70;
+
+    static final int CENTER_BARCODE_TOP_LEFT_X_ANCHOR_POINT = 320/2 - REGION_WIDTH/2;
+
+    static final Point LEFT_BARCODE_TOP_LEFT_ANCHOR_POINT =   new Point(0, 100);
+    static final Point CENTER_BARCODE_TOP_LEFT_ANCHOR_POINT = new Point(CENTER_BARCODE_TOP_LEFT_X_ANCHOR_POINT, 100);
+    static final Point RIGHT_BARCODE_TOP_LEFT_ANCHOR_POINT =  new Point(320-REGION_WIDTH, 100);
+
+
+    ////* Creating the sample regions *////
+    /* pointA = The Top left point of the region */
+    /* pointB = The Bottom right point of the region */
+
+    // Left Barcode
+    static final Rect LEFT_BARCODE = new Rect(
+            new Point(LEFT_BARCODE_TOP_LEFT_ANCHOR_POINT.x,
+                    LEFT_BARCODE_TOP_LEFT_ANCHOR_POINT.y),
+            new Point(LEFT_BARCODE_TOP_LEFT_ANCHOR_POINT.x + REGION_WIDTH,
+                    LEFT_BARCODE_TOP_LEFT_ANCHOR_POINT.y + REGION_HEIGHT));
+
+    // Center Barcode
+    static final Rect CENTER_BARCODE = new Rect(
+            new Point(CENTER_BARCODE_TOP_LEFT_ANCHOR_POINT.x,
+                    CENTER_BARCODE_TOP_LEFT_ANCHOR_POINT.y),
+            new Point(CENTER_BARCODE_TOP_LEFT_ANCHOR_POINT.x + REGION_WIDTH,
+                    CENTER_BARCODE_TOP_LEFT_ANCHOR_POINT.y + REGION_HEIGHT));
+
+    // Right Barcode
+    static final Rect RIGHT_BARCODE = new Rect(
+            new Point(RIGHT_BARCODE_TOP_LEFT_ANCHOR_POINT.x,
+                    RIGHT_BARCODE_TOP_LEFT_ANCHOR_POINT.y),
+            new Point(RIGHT_BARCODE_TOP_LEFT_ANCHOR_POINT.x + REGION_WIDTH,
+                    RIGHT_BARCODE_TOP_LEFT_ANCHOR_POINT.y + REGION_HEIGHT));
+
+    // Variable Declaration
+    Mat leftBarcode;
+    Mat centerBarcode;
+    Mat rightBarcode;
+    public double leftValue;
+    public double centerValue;
+    public double rightValue;
+
+    /**
+     * This function draw a Rectangle that is than displayed on the screen
+     * @param input Camera input
+     */
+    void drawRectangle (Mat input, Rect rect, Scalar colour, int thickness) {
+        // Left Barcode Rectangle
+        Imgproc.rectangle(
+                input, // Buffer to draw on
+                rect, // Where the Rectangle is drawn
+                colour, // The colour of the rectangle is drawn in
+                thickness); // Thickness of the rectangle lines
+    }
+
+    //Outputs
+    private final Mat resizeImageOutput = new Mat();
+    private final Mat cvCvtcolorOutput = new Mat();
+    private final Mat cvExtractchannelOutput = new Mat();
+    private final Mat cvThresholdOutput = new Mat();
+
+
+    /**
+     * Scales and image to an exact size.
+     * @param input The image on which to perform the Resize.
+     * @param width The width of the output in pixels.
+     * @param height The height of the output in pixels.
+     * @param interpolation The type of interpolation.
+     * @param output The image in which to store the output.
+     */
+    private void resizeImage(Mat input, double width, double height,
+                             int interpolation, Mat output) {
+        Imgproc.resize(input, output, new Size(width, height), 0.0, 0.0, interpolation);
+    }
+
+    /**
+     * Converts an image from one color space to another.
+     * @param src Image to convert.
+     * @param code conversion code.
+     * @param dst converted Image.
+     */
+    private void cvCvtcolor(Mat src, int code, Mat dst) {
+        Imgproc.cvtColor(src, dst, code);
+    }
+
+    /**
+     * Extracts given channel from an image.
+     * @param src the image to extract.
+     * @param channel zero indexed channel number to extract.
+     * @param dst output image.
+     */
+    private void cvExtractchannel(Mat src, double channel, Mat dst) {
+        Core.extractChannel(src, dst, (int)channel);
+    }
+
+    /**
+     * Apply a fixed-level threshold to each array element in an image.
+     * @param src Image to threshold.
+     * @param threshold threshold value.
+     * @param maxVal Maximum value for THRES_BINARY and THRES_BINARY_INV
+     * @param type Type of threshold to appy.
+     * @param dst output Image.
+     */
+    private void cvThreshold(Mat src, double threshold, double maxVal, int type,
+                             Mat dst) {
+        Imgproc.threshold(src, dst, threshold, maxVal, type);
+    }
+
+
+    /**
+     * This is the primary method that runs the entire pipeline and updates the outputs.
+     */
+    public void process(Mat source0) {
+        // Step Resize_Image0:
+        double resizeImageWidth = 320.0;
+        double resizeImageHeight = 240.0;
+        int resizeImageInterpolation = Imgproc.INTER_CUBIC;
+        resizeImage(source0, resizeImageWidth, resizeImageHeight, resizeImageInterpolation, resizeImageOutput);
+
+        // Step CV_cvtColor0:
+        int cvCvtcolorCode = Imgproc.COLOR_RGB2HSV;
+        cvCvtcolor(resizeImageOutput, cvCvtcolorCode, cvCvtcolorOutput);
+
+        // Step CV_extractChannel0:
+        double cvExtractchannelChannel = 0.0;
+        cvExtractchannel(cvCvtcolorOutput, cvExtractchannelChannel, cvExtractchannelOutput);
+
+        // Step CV_Threshold0:
+        double cvThresholdThresh = 89.0;
+        double cvThresholdMaxval = 255.0;
+        int cvThresholdType = Imgproc.THRESH_BINARY;
+        cvThreshold(cvExtractchannelOutput, cvThresholdThresh, cvThresholdMaxval, cvThresholdType, cvThresholdOutput);
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+
+        process(input);
+
+        leftBarcode = cvThresholdOutput.submat(LEFT_BARCODE);
+        centerBarcode = cvThresholdOutput.submat(CENTER_BARCODE);
+        rightBarcode = cvCvtcolorOutput.submat(RIGHT_BARCODE);
+
+        // Setting variable values
+        leftValue = (int) Core.mean(leftBarcode).val[0];
+        centerValue = (int) Core.mean(centerBarcode).val[0];
+        rightValue = (int) Core.mean(rightBarcode).val[0];
+
+        drawRectangle(input, LEFT_BARCODE, WHITE,2); // Left Barcode Rectangle
+        drawRectangle(input, CENTER_BARCODE, WHITE,2); // Left Barcode Rectangle
+        drawRectangle(input, RIGHT_BARCODE, WHITE,2); // Left Barcode Rectangle
+
+
+        double maxValue = leftValue;
+        elementPosition = 1;
+
+        if (centerValue > maxValue)
+        {
+            maxValue = centerValue;
+            elementPosition = 2;
+        }
+
+        if (rightValue > maxValue)
+        {
+            maxValue = rightValue;
+            elementPosition = 3;
+        }
+
+        if (ELEMENT_THRESHOLD > maxValue)
+        {
+            elementPosition = 0;
+        }
+
+
+        if (elementPosition == 1)
+        {
+            drawRectangle(input, LEFT_BARCODE, GREEN,3); // Left Barcode Rectangle
+        } else if (elementPosition == 2)
+        {
+            drawRectangle(input, CENTER_BARCODE, GREEN,3); // Center Barcode Rectangle
+        } else if (elementPosition == 3)
+        {
+            drawRectangle(input, RIGHT_BARCODE, GREEN,3); // Right Barcode Rectangle
+        }
+
+        // Return Input
+        return input;
+    }
+
+    /**
+     * Returns the position of the element. 1 = Left (level one,) 2 = Center (level two,) 3 = Right (level three)
+     * @return elementPosition
+     */
+    public int getElementPosition ()
+    {
+        return elementPosition;
+    }
+
+    /**
+     * Turns off and disables camera view.
+     */
+    public void disableWebcam () {
+        webcam.stopStreaming();
+        webcam.closeCameraDevice();
+    }
 }
+
