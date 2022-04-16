55b7a3668ba9bba0cb86678820be8d0fb1a46937
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2022-04-16 04:28:15 -0700

    Tuned Gyro Kp, will need to retune with field tiles.

--- TeamCode/src/main/java/teamcode/Season_Setup/RobotParams.java
+++ TeamCode/src/main/java/teamcode/Season_Setup/RobotParams.java
@@ -172,7 +172,7 @@ public class RobotParams
     public static final double ENCODER_Y_TOLERANCE                     = 1.0;
     public static final double ENCODER_Y_INCHES_PER_COUNT              = 0.05945352709657886769603390856797;
 
-    public static final double GYRO_KP                                 = 0.003;
+    public static final double GYRO_KP                                 = 0.015;
     public static final double GYRO_KI                                 = 0.0;
     public static final double GYRO_KD                                 = 0.000;
     public static final double GYRO_TOLERANCE                          = 2.0;
