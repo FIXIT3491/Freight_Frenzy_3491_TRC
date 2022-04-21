ee8d26940bc10253059f6b28515a5c6430f0eff9
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2022-04-20 23:23:20 -0500

    Program to just drive forward and score at correct level, currently not working, issue unknown.

--- TeamCode/src/main/java/teamcode/Competitions/Autonomous/PurePursuit_CmdAutoFarCarousel.java
+++ TeamCode/src/main/java/teamcode/Competitions/Autonomous/PurePursuit_CmdAutoFarCarousel.java
@@ -23,9 +23,11 @@
 package teamcode.Competitions.Autonomous;
 
 import TrcCommonLib.trclib.TrcEvent;
+import TrcCommonLib.trclib.TrcPose2D;
 import TrcCommonLib.trclib.TrcRobot;
 import TrcCommonLib.trclib.TrcStateMachine;
 import TrcCommonLib.trclib.TrcTimer;
+import teamcode.Season_Setup.Freight_Frenzy_Pipeline;
 import teamcode.Season_Setup.Robot;
 import teamcode.Season_Setup.RobotParams;
 
@@ -33,30 +35,25 @@ class PurePursuit_CmdAutoFarCarousel implements TrcRobot.RobotCommand
 {
     private static final String moduleName = "CmdAutoFarCarousel";
 
-    private enum State
-    {
-        START_DELAY,
-        DRIVE_TO_ALLIANCE_SHIPPING_HUB,
-        DEPOSIT_FREIGHT,
-
-        DRIVE_TO_CAROUSEL,
-        GET_TO_CAROUSEL,
-        SPIN_CAROUSEL,
-
-        DRIVE_TO_ALLIANCE_STORAGE_UNIT,
-        DRIVE_TO_WAREHOUSE,
-        GET_INTO_WAREHOUSE,
-
-        DONE
-    }   // enum State
-
     private final Robot robot;
     private final FtcAuto.AutoChoices autoChoices;
     private final TrcTimer timer;
     private final TrcEvent event;
     private final TrcStateMachine<State> sm;
-    private int elementPosition = 0;
-    private Double expireTime = null;
+    private int elementPosition;
+    Freight_Frenzy_Pipeline freight_frenzy_pipeline = new Freight_Frenzy_Pipeline();
+
+    private enum State
+    {
+        START_DELAY,
+
+        DRIVE_TO_ALLIANCE_SHIPPING_HUB,
+        DEPOSIT_FREIGHT,
+
+        DRIVE_BACK,
+
+        DONE
+    }   // enum State
 
     /**
      * Constructor: Create an instance of the object.
@@ -123,11 +120,109 @@ class PurePursuit_CmdAutoFarCarousel implements TrcRobot.RobotCommand
             String msg;
 
             robot.dashboard.displayPrintf(1, "State: %s", state);
-            switch (state)
-            {
-              
-            }
+            switch (state) {
+                case START_DELAY:
+                    // Set robot starting position in the field.
+                    robot.robotDrive.driveBase.setFieldPosition(
+                            autoChoices.alliance == FtcAuto.Alliance.RED_ALLIANCE?
+                                    RobotParams.STARTPOS_RED_NEAR_CAROUSEL : RobotParams.STARTPOS_BLUE_NEAR_CAROUSEL);
 
+                    // Call vision at the beginning to figure out the position of the duck.
+                    if (robot.vision != null)
+                    {
+                        elementPosition = freight_frenzy_pipeline.getPosition().value;
+                    }
+
+                    if (freight_frenzy_pipeline.getPosition().value == 0)
+                    {
+                        // We still can't see the element, default to level 3.
+                        elementPosition = 3;
+                        msg = "No element found, default to position " + elementPosition;
+                        robot.globalTracer.traceInfo(moduleName, msg);
+
+                        robot.speak(msg);
+                    }
+                    else
+                    {
+                        msg = "Element found at position " + elementPosition;
+                        robot.globalTracer.traceInfo(moduleName, msg);
+
+                        robot.speak("Element found at position " + elementPosition);
+                    }
+
+                    // Do start delay if any.
+                    if (autoChoices.startDelay == 0.0)
+                    {
+                        // Intentionally falling through to the next state.
+                        sm.setState(State.DRIVE_TO_ALLIANCE_SHIPPING_HUB);
+                    }
+                    else
+                    {
+                        timer.set(autoChoices.startDelay, event);
+                        sm.waitForSingleEvent(event, State.DRIVE_TO_ALLIANCE_SHIPPING_HUB);
+                        break;
+                    }
+
+                case DRIVE_TO_ALLIANCE_SHIPPING_HUB:
+
+                    // Note: the smaller the number the closer to the hub.
+                    double distanceToHub = elementPosition == 3? 5.0: elementPosition == 2? 5.0: 5.0;
+
+                    // Drive to the alliance specific hub from the starting position.
+                    if (autoChoices.alliance == FtcAuto.Alliance.RED_ALLIANCE)
+                    {
+
+                        robot.robotDrive.driveBase.setFieldPosition(new TrcPose2D(0.0, 0.0, 0.0));
+
+                        robot.robotDrive.purePursuitDrive.setMoveOutputLimit(0.5);
+
+                        robot.robotDrive.pidDrive.setRelativeTarget(
+                                0.0,36.0,0.0, event, 5.0);
+
+                        robot.armPlatformRotator.setLevel(1);
+                    }
+                    else
+                    {
+                        TrcPose2D firstPoint = robot.robotDrive.pathPoint(0.5, 1.0, 180.0);
+
+                        robot.robotDrive.purePursuitDrive.start(
+                                event, robot.robotDrive.driveBase.getFieldPosition(), false,
+                                firstPoint);
+
+                        robot.armPlatformRotator.setLevel(1);
+                    }
+
+                    // Raise arm to the detected duck level at the same time.
+                    robot.armRotator.setLevel(elementPosition);
+                    robot.armExtender.setLevel(2);
+
+                    sm.waitForSingleEvent(event, State.DEPOSIT_FREIGHT);
+
+                    break;
+
+                case DEPOSIT_FREIGHT:
+                    // Dumps the freight, when done, signals event and goes to next state.
+                    robot.collector.setPosition(RobotParams.COLLECTOR_DEPOSIT_POWER, RobotParams.COLLECTOR_DEPOSITING_TIME, event);
+
+                    sm.waitForSingleEvent(event, State.DRIVE_BACK);
+                    break;
+
+                case DRIVE_BACK:
+
+                    robot.robotDrive.driveBase.tankDrive(-0.4, -0.4, false);
+                    timer.set(0.5, event);
+
+                    sm.waitForSingleEvent(event, State.DONE);
+
+                    break;
+
+                case DONE:
+                default:
+                    // We are done, zero calibrate the arm will lower it.
+                    robot.armRotator.zeroCalibrate();
+                    cancel();
+                    break;
+            }
 
             if (traceState)
             {
@@ -138,6 +233,6 @@ class PurePursuit_CmdAutoFarCarousel implements TrcRobot.RobotCommand
         }
 
         return !sm.isEnabled();
-    }   //cmdPeriodic
+    }   // cmdPeriodic
 
-}   //class CmdAutoFarCarousel
+}   // class CmdAutoNearCarousel
